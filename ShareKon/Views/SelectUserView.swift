4130fc1e06d00d39a524228c5ce827aba74ce11c
A T <c5aaa.p3v@icloud.com>
2026-01-24 18:49:21 +0900

    デッドコード削除

--- ShareKon/Views/SelectUserView.swift
+++ ShareKon/Views/SelectUserView.swift
@@ -11,10 +11,7 @@ struct SelectUserView: View {
     @State private var addUser = "" // テキストフィールドの値を保持
     @Binding var users: [String]// リストに表示するカテゴリの配列
     @Binding var selectedUser: String?       // 親画面から渡される選択されたカテゴリ
-    @State private var showCategoryList = false  // カテゴリ一覧画面を表示するかどうかのフラグ
-    @Environment(\.dismiss) private var dismiss  // 前の画面に戻るための環境変数
-    @State private var pressedCategory: String? = nil  // 押下中のカテゴリを保持して押下感を表現
-    @EnvironmentObject var categoryModel: CategoryModel
+
     var body: some View {
         CommonAddLayout(
             title: "ユーザ一覧",
